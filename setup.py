6fd47329a27d3eabdfdae9cbefb49bc1b4ef49f7
Didier Roche <didrocks@ubuntu.com>
2014-06-06 13:13:13 +0200

    Add first release medatada and move scripts
    * Move developer-tools-center to __init__.py to then generate the script
    * Add a helper to run from master
    * Add setup.py to deploy the tools. Ship only files we want to ship.
      However, the work isn't fully done yet:
      - data_dir seems to not be shipped in the correct directory in
        setup.py install (without --root)
      - Run whole test suite, we maybe want to run only small tests
      - gather correct requirements as per target, the install_requires doesn't
        seem to have any effect though
      -  add babel for handling i18n and l10n, not bound to the release process yet
    * Some initial function to get the data directory. Need to fetch version and
      change destination when installing though.

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Canonical
+#
+# Authors:
+#  Didier Roche
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from babel.messages import frontend as babel
+from glob import glob
+from setuptools import setup, find_packages
+
+
+def get_requirements(tag_to_detect=""):
+    """Gather a list line per line of requirements from tag_to_detect to next tag.
+
+    if tag_to_detect is empty, it will gather every requirements"""
+    requirements = []
+    tag_detected = False
+    with open("requirements.txt") as f:
+        for line in f.read().splitlines():
+            if line.startswith("#") or line == "":
+                tag_detected = False
+                if line.startswith(tag_to_detect):
+                    tag_detected = True
+                continue
+            if tag_detected:
+                requirements.append(line)
+    return requirements
+
+setup(
+    name="Ubuntu Developer Tools Center",
+    version="0.0.1",
+    packages=find_packages(exclude=["tests*"]),
+    package_data={},
+    entry_points={
+        'console_scripts': [
+            'developer-tools-center = udtc:main'
+        ],
+    },
+
+    data_files=[("share/developer-tools-center/log-confs", glob('log-confs/*.yaml'))],
+
+    # TODO: install_requires doesn't seem to be force on setup.py install
+    install_requires=get_requirements("# runtime"),
+    setup_requires=get_requirements(),
+    tests_require=get_requirements("# test"),
+
+    # In addition to run all nose tests, that will as well show python warnings
+    test_suite="nose.collector",
+
+    # i18n based on http://babel.pocoo.org/docs/setup/
+    cmdclass={'compile_catalog': babel.compile_catalog,
+              'extract_messages': babel.extract_messages,
+              'init_catalog': babel.init_catalog,
+              'update_catalog': babel.update_catalog}
+)
--- udtc/__init__.py
+++ udtc/__init__.py
@@ -17,18 +17,24 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-
+import argparse
+import gettext
+from gettext import gettext as _
 import logging
 import logging.config
 import os
+from textwrap import dedent
+from udtc.network.download_center import DownloadCenter
 import yaml
 
-_default_log_level = logging.WARNING
-
+gettext.textdomain("ubuntu-developer-tools-center")
 logger = logging.getLogger(__name__)
 
+_default_log_level = logging.WARNING
+_datadir = None
 
-def setup_logging(default_file='logging.yaml', env_key='LOG_CFG', level=_default_log_level):
+
+def _setup_logging(default_file='logging.yaml', env_key='LOG_CFG', level=_default_log_level):
     """Setup logging configuration
 
     Order of preference:
@@ -48,3 +54,40 @@ def setup_logging(default_file='logging.yaml', env_key='LOG_CFG', level=_default
                 config = yaml.load(f.read())
             logging.config.dictConfig(config)
     logger.info("Logging level set to {}".format(logging.getLevelName(level)))
+
+
+def get_data_dir():
+    """Return absolute path to data dir.
+
+    Use the local data dir if found, otherwise, return the system dir.
+    """
+    if not _datadir:
+        local_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+        if os.path.isdir(local_dir):
+            logger.debug("Using local data dir in {}".format(local_dir))
+            return local_dir
+    if not _datadir:
+        logging.error("No data directory found")
+    return _datadir
+
+
+def main():
+    """Main entry point of the program"""
+
+    parser = argparse.ArgumentParser(description=_("Deploy and setup developers environment easily on ubuntu"),
+                                     epilog=dedent(_("""\
+                                         Note that you can also configure different debug logs behaviors using LOG_CFG
+                                         pointing to a log yaml profile.
+                                         """)))
+    parser.add_argument("-v", "--verbose", action="count", default=0, help=_("increase output verbosity (2 levels)"))
+    args = parser.parse_args()
+
+    # setup logging level if set by the command line
+    if args.verbose == 1:
+        _setup_logging(level=logging.INFO)
+    elif args.verbose > 1:
+        _setup_logging(level=logging.DEBUG)
+    else:
+        _setup_logging()
+
+    DownloadCenter(["http://victorlin.me/posts/2012/08/26/good-logging-practice-in-python"], lambda x: print(x))
