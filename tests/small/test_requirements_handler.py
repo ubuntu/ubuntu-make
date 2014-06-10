153dffc9097b6c7b52af1840ae272967a28bc77e
Didier Roche <didrocks@ubuntu.com>
2014-06-10 10:12:30 +0200

    Add new Requirements handler module
    Test branch to pass Travis requirements

---
+++ tests/small/test_requirements_handler.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Canonical
+#
+# Authors:
+#  Didier Roche
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Tests for the download center module using a local server"""
+
+from unittest import TestCase
+from udtc.network.requirements_handler import RequirementsHandler
+
+
+class TestRequirementsHandler(TestCase):
+    """This will test the download center by sending one or more download requests"""
+
+    def test_foo(self):
+        """First test to ensure we can at least import the right apt bindings through Travis"""
+        pass
