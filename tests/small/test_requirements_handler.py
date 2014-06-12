b0496e58be024b33459a0c1351b914e35480d877
Didier Roche <didrocks@ubuntu.com>
2014-06-12 10:33:56 +0200

    Add more tests for RequirementsHandler
    Also, use the parameter for signaling the timeout in case of error.

--- tests/small/test_requirements_handler.py
+++ tests/small/test_requirements_handler.py
@@ -99,7 +99,7 @@ class TestRequirementsHandler(TestCase):
         timeout_time = time() + timeout
         while not mock_function_to_be_called.called:
             if time() > timeout_time:
-                raise(BaseException("Function not called within 10 seconds"))
+                raise(BaseException("Function not called within {} seconds".format(timeout)))
 
     def test_singleton(self):
         """Ensure we are delivering a singleton for RequirementsHandler"""
@@ -152,30 +152,110 @@ class TestRequirementsHandler(TestCase):
         self.assertTrue(downloading_msg > 1)
         self.assertTrue(installing_msg > 1)
 
+    def test_install_pending(self):
+        """Appending two installations and wait for results. Only the first call should have progress"""
+        done_callback0 = Mock()
+        self.handler.install_bucket(["testpackage"], lambda x, y: "", self.done_callback)
+        self.handler.install_bucket(["testpackage0"], lambda x, y: "", done_callback0)
+        self.wait_for_callback(self.done_callback)
+        self.wait_for_callback(done_callback0)
+
+        self.assertTrue(self.handler.cache["testpackage"].is_installed)
+        self.assertTrue(self.handler.cache["testpackage0"].is_installed)
+
+    def test_install_pending_order(self):
+        """Installation order of pending requests are respected"""
+        done_callback = Mock()
+        done_callback.side_effect = self.done_callback
+        done_callback0 = Mock()
+        done_callback0.side_effect = self.done_callback
+        ordered_progress_callback = Mock()
+        progress_callback = Mock()
+        progress_callback.side_effect = ordered_progress_callback
+        progress_callback0 = Mock()
+        progress_callback0.side_effect = ordered_progress_callback
+        self.handler.install_bucket(["testpackage"], progress_callback, done_callback)
+        self.handler.install_bucket(["testpackage0"], progress_callback0, done_callback0)
+        self.wait_for_callback(done_callback)
+        self.wait_for_callback(done_callback0)
+
+        self.assertEqual(self.done_callback.call_args_list,
+                         [call({'bucket': ['testpackage'], 'error': None}),
+                          call({'bucket': ['testpackage0'], 'error': None})])
+        # we will get progress with 0, 1 (first bucket), 0, 1 (second bucket). So 4 progress signal status change
+        current_status = RequirementsHandler.STATUS_DOWNLOADING
+        current_status_change_count = 1
+        calls = ordered_progress_callback.call_args_list
+        for i in range(len(calls)):
+            if i == 0:
+                continue
+            if calls[i][0][0] != current_status:
+                current_status = calls[i][0][0]
+                current_status_change_count += 1
+        self.assertEqual(current_status_change_count, 4)
+
+    def test_install_pending_callback_not_mixed(self):
+        """Callbacks are separated on pending requests"""
+        done_callback = Mock()
+        done_callback.side_effect = self.done_callback
+        done_callback0 = Mock()
+        done_callback0.side_effect = self.done_callback
+        global_progress_callback = Mock()
+        progress_callback = Mock()
+        progress_callback.side_effect = global_progress_callback
+        progress_callback0 = Mock()
+        progress_callback0.side_effect = global_progress_callback
+        self.handler.install_bucket(["testpackage"], progress_callback, done_callback)
+        self.handler.install_bucket(["testpackage0"], progress_callback0, done_callback0)
+        self.wait_for_callback(done_callback)
+        self.wait_for_callback(done_callback0)
+
+        self.assertTrue(progress_callback.call_count < global_progress_callback.call_count)
+        self.assertTrue(progress_callback0.call_count < global_progress_callback.call_count)
+        self.assertTrue(done_callback.call_count < self.done_callback.call_count)
+        self.assertTrue(done_callback0.call_count < self.done_callback.call_count)
+
     def test_install_twice(self):
-        """Test appending two installations and wait for results"""
-        self.handler.install_bucket(["testpackage"], lambda x, y: "", lambda x: "")
+        """Test appending two installations and wait for results. Only the first call should have progress"""
+        progress_callback = Mock()
+        progress_second_callback = Mock()
+        done_callback = Mock()
+        self.handler.install_bucket(["testpackage"], progress_callback, done_callback)
+        self.handler.install_bucket(["testpackage"], progress_second_callback, self.done_callback)
+        self.wait_for_callback(done_callback)
+        self.wait_for_callback(self.done_callback)
+
+        self.assertTrue(self.handler.cache["testpackage"].is_installed)
+        self.assertFalse(progress_second_callback.called)
+
+    def test_deps(self):
+        """Installing one package, ensure the dep (even with auto_fix=False) is installed"""
         self.handler.install_bucket(["testpackage1"], lambda x, y: "", self.done_callback)
         self.wait_for_callback(self.done_callback)
 
-        # TODO: ensure done callback one is before done callback2
-
-    def test_install_twice_same_packages(self):
-
-        # TODO: ensure that progress isn't called the second time?
-        pass
+        self.assertTrue(self.handler.cache["testpackage1"].is_installed)
+        self.assertTrue(self.handler.cache["testpackage"].is_installed)
 
     def test_fail(self):
-        pass
+        """Raise an error when asking for the impossible (installing 2 packages in conflicts)"""
+        self.handler.install_bucket(["testpackage", "testpackage2"], lambda x, y: "", self.done_callback)
+        self.wait_for_callback(self.done_callback)
 
-    def test_package_doesnt_exists(self):
-        pass
+        self.assertIsNotNone(self.done_callback.call_args[0][0]["error"])
+        self.assertTrue(self.handler.cache["testpackage"].is_installed)
+        self.assertFalse(self.handler.cache["testpackage2"].is_installed)
+
+    def test_install_shadow_pkg(self):
+        """Raise an error if we try to install a none existing package"""
+        self.handler.install_bucket(["foo"], lambda x, y: "", self.done_callback)
+        self.wait_for_callback(self.done_callback)
+
+        self.assertIsNotNone(self.done_callback.call_args[0][0]["error"])
 
     def test_error_in_dpkg(self):
         """Test that an error while installing a package is caught"""
-        # try an error on dpkg
         with open(self.dpkg, mode='w') as f:
-            f.write("#!/bin/sh\nexit 1")
+            f.write("#!/bin/sh\nexit 1")  # Simulate an error in dpkg
         self.handler.install_bucket(["testpackage"], lambda x, y: "", self.done_callback)
         self.wait_for_callback(self.done_callback)
 
