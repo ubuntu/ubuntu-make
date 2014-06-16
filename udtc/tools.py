f874cc681f3f4b6ece20104d72836f0d826046ca
Didier Roche <didrocks@ubuntu.com>
2014-06-16 11:22:31 +0200

    Fix pep8 in tools.py

--- udtc/tools.py
+++ udtc/tools.py
@@ -19,7 +19,9 @@
 
 
 class Singleton(type):
+
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
