6230583a16ecb8eab8a853a79a29d667432e5f20
Tin Tvrtkovic <tinchester@gmail.com>
2014-09-23 18:31:32 +0200

    Changes motivated by comments at https://github.com/didrocks/ubuntu-developer-tools-center/pull/26.

--- udtc/frameworks/ide.py
+++ udtc/frameworks/ide.py
@@ -23,40 +23,39 @@
 
 from gettext import gettext as _
 import logging
+import platform
 
 from os.path import join
 import udtc.frameworks.baseinstaller
 from udtc.network.download_center import DownloadCenter
 from udtc.tools import create_launcher, get_application_desktop_file
+from udtc.ui import UI
 
 
 logger = logging.getLogger(__name__)
-
-_supported_archs = ['i386', 'amd64']
 
 
 class IdeCategory(udtc.frameworks.BaseCategory):
     def __init__(self):
         super().__init__(name=_("IDE"), description=_("Generic IDEs"),
-                         logo_path=None,
-                         packages_requirements=["openjdk-7-jdk"])
+                         logo_path=None)
 
 
-class EclipseLuna(udtc.frameworks.baseinstaller.BaseInstaller):
-    """The Eclipse Foundation 4.4 distribution."""
+class Eclipse(udtc.frameworks.baseinstaller.BaseInstaller):
+    """The Eclipse Foundation distribution."""
     DOWNLOAD_URL_PAT = "https://www.eclipse.org/downloads/download.php?" \
                        "file=/technology/epp/downloads/release/luna/R/" \
-                       "eclipse-standard-luna-R-linux-gtk-{arch}.tar.gz{suf}" \
+                       "eclipse-standard-luna-R-linux-gtk{arch}.tar.gz{suf}" \
                        "&r=1"
 
     def __init__(self, category):
-        self.desktop_filename = "eclipse-luna.desktop"
-        super().__init__(name="Eclipse Luna",
-                         description="Pure Eclipse Luna (4.4)",
-                         category=category, only_on_archs=_supported_archs,
+        super().__init__(name=_("Eclipse"),
+                         description=_("Pure Eclipse Luna (4.4)"),
+                         category=category, only_on_archs=['i386', 'amd64'],
                          download_page=None,
                          dir_to_decompress_in_tarball='eclipse',
-                         desktop_file_name=self.desktop_filename)
+                         desktop_filename='eclipse-luna.desktop',
+                         packages_requirements=['openjdk-7-jdk'])
 
     def download_provider_page(self):
         """First, we need to fetch the MD5, then kick off the proceedings.
@@ -64,23 +63,34 @@ class EclipseLuna(udtc.frameworks.baseinstaller.BaseInstaller):
         This could actually be done in parallel, in a future version.
         """
         logger.debug("Preparing to download MD5.")
-        self.mark_in_config()
 
-        md5_url = self.DOWNLOAD_URL_PAT.format(arch='x86_64', suf='.md5')
+        arch = platform.machine()
+        if arch == 'i386':
+            md5_url = self.DOWNLOAD_URL_PAT.format(arch='', suf='.md5')
+        elif arch == 'x86_64':
+            md5_url = self.DOWNLOAD_URL_PAT.format(arch='-x86_64', suf='.md5')
+        else:
+            logger.error("Unsupported architecture: {}".format(arch))
+            UI.return_main_screen()
 
         def done(download_result):
             res = download_result[md5_url]
 
             if res.error:
                 logger.error(res.error)
-                return  # Do something else?
+                UI.return_main_screen()
+                return
 
             # Should be ASCII anyway.
             md5 = res.buffer.getvalue().decode('utf-8').split()[0]
             logger.debug("Downloaded MD5 is {}".format(md5))
 
             logger.debug("Preparing to download the main archive.")
-            download_url = self.DOWNLOAD_URL_PAT.format(arch='x86_64', suf='')
+            if arch == 'i386':
+                download_url = self.DOWNLOAD_URL_PAT.format(arch='', suf='')
+            elif arch == 'x86_64':
+                download_url = self.DOWNLOAD_URL_PAT.format(arch='-x86_64',
+                                                            suf='')
             self.download_requests.append((download_url, md5))
             self.start_download_and_install()
 
@@ -88,15 +98,13 @@ class EclipseLuna(udtc.frameworks.baseinstaller.BaseInstaller):
 
     def create_launcher(self):
         """Create the Luna launcher"""
-        # Icon paths don't work with quotes?
         icon_path = join(self.install_path, "icon.xpm")
         exec_path = '"{}" %f'.format(join(self.install_path, "eclipse"))
         create_launcher(self.desktop_filename,
                         get_application_desktop_file(name=_("Eclipse Luna"),
                                                      icon_path=icon_path,
                                                      exec=exec_path,
-                                                     comment=_(
-                                                         "The Eclipse Luna Integrated Development Environment"),
+                                                     comment=_("The Eclipse Luna Integrated Development Environment"),
                                                      categories="Development;IDE;"))
 
     @property
@@ -108,3 +116,7 @@ class EclipseLuna(udtc.frameworks.baseinstaller.BaseInstaller):
             logger.debug("{} binary isn't installed".format(self.name))
             return False
         return True
+
+    def download_done(self, result):
+        super().download_done(result)
+        self.mark_in_config()
