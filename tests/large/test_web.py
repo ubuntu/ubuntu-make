dfed0526a743c2490d25479e98c24c5136bf36c1
Didier Roche <didrocks@ubuntu.com>
2015-04-30 12:53:45 +0200

    Add large test for Visual Studio Code

--- tests/large/test_web.py
+++ tests/large/test_web.py
@@ -83,3 +83,54 @@ class FirefoxDevTests(LargeFrameworkTests):
         self.expect_and_no_warn("Firefox Dev is already installed.*\[.*\] ")
         self.child.sendline()
         self.wait_and_no_warn()
+
+
+class VisualStudioCodeTest(LargeFrameworkTests):
+    """Tests for Visual Studio Code"""
+
+    TIMEOUT_INSTALL_PROGRESS = 120
+    TIMEOUT_START = 20
+    TIMEOUT_STOP = 20
+
+    def setUp(self):
+        super().setUp()
+        self.installed_path = os.path.expanduser("~/tools/web/visual-studio-code")
+        self.desktop_filename = "visual-studio-code.desktop"
+        self.icon_filename = "vso.png"
+
+    @property
+    def full_icon_path(self):
+        return join(self.installed_path, "resources", "app", self.icon_filename)
+
+    @property
+    def exec_path(self):
+        return os.path.join(self.installed_path, "Code")
+
+    def test_default_install(self):
+        """Install firefox dev from scratch test case"""
+        self.child = pexpect.spawnu(self.command('{} web visual-studio-code'.format(UMAKE)))
+        self.expect_and_no_warn("Choose installation path: {}".format(self.installed_path))
+        self.child.sendline("")
+        self.expect_and_no_warn("\[I Accept.*\]")  # ensure we have a license question
+        self.child.sendline("a")
+        self.expect_and_no_warn("Installation done", timeout=self.TIMEOUT_INSTALL_PROGRESS)
+        self.wait_and_no_warn()
+
+        # we have an installed launcher, added to the launcher and an icon file
+        self.assertTrue(self.launcher_exists_and_is_pinned(self.desktop_filename))
+        self.assertTrue(self.path_exists(self.exec_path))
+        self.assertTrue(self.path_exists(self.full_icon_path))
+
+        # launch it, send SIGTERM and check that it exits fine
+        proc = subprocess.Popen(self.command_as_list(self.exec_path), stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+
+        self.check_and_kill_process(["Code", self.installed_path],
+                                    wait_before=self.TIMEOUT_START, send_sigkill=True)
+        proc.wait(self.TIMEOUT_STOP)
+
+        # ensure that it's detected as installed:
+        self.child = pexpect.spawnu(self.command('{} web visual-studio-code'.format(UMAKE)))
+        self.expect_and_no_warn("Visual Studio Code is already installed.*\[.*\] ")
+        self.child.sendline()
+        self.wait_and_no_warn()
