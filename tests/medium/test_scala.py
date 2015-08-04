fc87614c1817d8c107598545b4930ab643714187
Igor Vuk <parcijala@gmail.com>
2015-08-05 01:06:18 +0200

    Add the faked package and repository

--- tests/medium/test_scala.py
+++ tests/medium/test_scala.py
@@ -31,6 +31,7 @@ class ScalaInContainer(ContainerTests, test_scala.ScalaTests):
     def setUp(self):
         self.hostname = "www.scala-lang.org"
         self.port = "80"
+        self.apt_repo_override_path = os.path.join(self.APT_FAKE_REPO_PATH, 'scala')
         super().setUp()
         # override with container path
         self.installed_path = os.path.expanduser("/home/{}/tools/scala/scala-lang".format(self.DOCKER_USER))
