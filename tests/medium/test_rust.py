161d1f4d4f1194a5e0ab2fe78529d0c15a54132d
Jared Ravetch <jravetch@gmail.com>
2015-11-04 15:14:00 -0800

    Add name to authors

--- tests/medium/test_rust.py
+++ tests/medium/test_rust.py
@@ -3,6 +3,7 @@
 #
 # Authors:
 #  Didier Roche
+#  Jared Ravetch
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
