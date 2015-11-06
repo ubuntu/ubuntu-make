7648888cfdba0b4dd6a53c59c344d9ce108d10a1
Didier Roche <didrocks@ubuntu.com>
2015-11-06 10:45:49 +0100

    Add couple of medium rust tests for errors
    As rust isn't using the BaseInstaller frameworks, ensure that modified
    download page and wrong sha are detected as expected.

--- tests/medium/test_rust.py
+++ tests/medium/test_rust.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014 Canonical
+# Copyright (C) 2014-2015 Canonical
 #
 # Authors:
 #  Didier Roche
@@ -22,14 +22,44 @@
 
 from . import ContainerTests
 import os
+import pexpect
+
 from ..large import test_rust
+from ..tools import get_data_dir, UMAKE, swap_file_and_restore, spawn_process
 
 
 class RustInContainer(ContainerTests, test_rust.RustTests):
     """This will test the Rust integration inside a container"""
+
+    TEST_CHECKSUM_RUST_DATA = "1eef6a898e0c8f3de9055aba7da2ff5ff1ef3aa139d8fea253687a06e32c5596"
 
     def setUp(self):
         self.hosts = {443: ["www.rust-lang.org"]}
         super().setUp()
         # override with container path
         self.installed_path = os.path.join(self.install_base_path, "rust", "rust-lang")
+
+    def test_install_with_changed_download_reference_page(self):
+        """Installing Rust should fail if download reference page has significantly changed"""
+        download_page_file_path = os.path.join(get_data_dir(), "server-content", "www.rust-lang.org", "downloads.html")
+        umake_command = self.command('{} rust'.format(UMAKE))
+        self.bad_download_page_test(umake_command, download_page_file_path)
+        self.assertFalse(self.path_exists(self.exec_path))
+
+    def test_install_with_wrong_sha(self):
+        """Installing Rust should fail if checksum is wrong"""
+        # we only modify the amd64 sha as docker only run on it
+        download_page_file_path = os.path.join(get_data_dir(), "server-content", "www.rust-lang.org",
+                                               "rust-fake-x86_64-unknown-linux-gnu.tar.gz.sha256")
+        with swap_file_and_restore(download_page_file_path) as content:
+            with open(download_page_file_path, "w") as newfile:
+                newfile.write(content.replace(self.TEST_CHECKSUM_RUST_DATA, "abcdef"))
+            self.child = spawn_process(self.command('{} rust'.format(UMAKE)))
+            self.expect_and_no_warn("Choose installation path: {}".format(self.installed_path))
+            self.child.sendline("")
+            self.expect_and_no_warn([pexpect.EOF, "Corrupted download? Aborting."],
+                                    timeout=self.TIMEOUT_INSTALL_PROGRESS, expect_warn=True)
+            self.wait_and_close(exit_status=1)
+
+            # we have nothing installed
+            self.assertFalse(self.path_exists(self.exec_path))
