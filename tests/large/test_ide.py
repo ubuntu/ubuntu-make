30c52dd65e0d99eb234cf104f0510e19658a5c4f
Didier Roche <didrocks@ubuntu.com>
2015-11-17 09:18:27 +0100

    Add a CLion large test

--- tests/large/test_ide.py
+++ tests/large/test_ide.py
@@ -406,6 +406,47 @@ class PhpStormIDETests(LargeFrameworkTests):
         self.wait_and_close()
 
 
+class CLionIDETests(LargeFrameworkTests):
+    """CLion test from the IDE collection"""
+
+    TIMEOUT_INSTALL_PROGRESS = 120
+    TIMEOUT_START = 60
+    TIMEOUT_STOP = 60
+
+    def setUp(self):
+        super().setUp()
+        self.installed_path = os.path.join(self.install_base_path, "ide", "clion")
+        self.desktop_filename = 'jetbrains-clion.desktop'
+
+    def test_default_install(self):
+        """Install from scratch test case"""
+        self.child = spawn_process(self.command('{} ide clion'.format(UMAKE)))
+        self.expect_and_no_warn("Choose installation path: {}".format(self.installed_path))
+        self.child.sendline("")
+        self.expect_and_no_warn("Installation done", timeout=self.TIMEOUT_INSTALL_PROGRESS)
+        self.wait_and_close()
+
+        logger.info("Installed, running...")
+
+        # we have an installed launcher, added to the launcher and an icon file
+        self.assertTrue(self.launcher_exists_and_is_pinned(self.desktop_filename))
+        self.assert_exec_exists()
+        self.assert_icon_exists()
+
+        # launch it, send SIGTERM and check that it exits fine
+        proc = subprocess.Popen(self.command_as_list(self.exec_path), stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+
+        self.check_and_kill_process(["java", self.installed_path], wait_before=self.TIMEOUT_START)
+        proc.wait(self.TIMEOUT_STOP)
+
+        # ensure that it's detected as installed:
+        self.child = spawn_process(self.command('{} ide clion'.format(UMAKE)))
+        self.expect_and_no_warn("CLion is already installed.*\[.*\] ")
+        self.child.sendline()
+        self.wait_and_close()
+
+
 class ArduinoIDETests(LargeFrameworkTests):
     """The Arduino Software distribution from the IDE collection."""
 
