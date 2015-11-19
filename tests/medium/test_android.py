5f28bebcdaf5d36b0c7bef6611af899491ff3b0b
Didier Roche <didrocks@ubuntu.com>
2015-11-19 08:54:51 +0100

    Fix bad description

--- tests/medium/test_android.py
+++ tests/medium/test_android.py
@@ -17,7 +17,7 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Tests for basic CLI commands"""
+"""Tests for android frameworks in container"""
 
 from . import ContainerTests
 import os
