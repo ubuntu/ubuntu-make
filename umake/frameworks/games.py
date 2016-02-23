90127ff26a0dfc0a495ccc06233bac7cdbabc6ae
Galileo Sartor <galileo.sartor@gmail.com>
2016-02-23 10:04:38 +0100

    Unity3D on lts mesa
    Added as possible dependencies mesa-lts
    Closes: #253

--- umake/frameworks/games.py
+++ umake/frameworks/games.py
@@ -123,7 +123,8 @@ class Unity3D(umake.frameworks.baseinstaller.BaseInstaller):
                          # don't want to create fake packages and kill the container for medium tests)
                          packages_requirements=["gconf-service", "lib32gcc1", "lib32stdc++6", "libasound2", "libcairo2",
                                                 "libcap2", "libcups2", "libfontconfig1", "libfreetype6", "libgconf-2-4",
-                                                "libgdk-pixbuf2.0-0", "libgl1-mesa-glx", "libglu1-mesa", "libgtk2.0-0",
+                                                "libgdk-pixbuf2.0-0", "libglu1-mesa", "libgtk2.0-0",
+                                                "libgl1-mesa-glx | libgl1-mesa-glx-lts-vivid | libgl1-mesa-glx-lts-wily",
                                                 "libnspr4", "libnss3", "libpango1.0-0", "libpq5", "libxcomposite1",
                                                 "libxcursor1", "libxdamage1", "libxext6", "libxfixes3", "libxi6",
                                                 "libxrandr2", "libxrender1", "libxtst6",
