beb0db49151a7a68e11024e90165ae6ae615c857
Trent Schafer <trent.schafer@gmail.com>
2016-09-29 10:57:05 +1000

    Update nodejs post install message

--- umake/frameworks/nodejs.py
+++ umake/frameworks/nodejs.py
@@ -85,7 +85,7 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
         add_env_to_user(self.name, {"PATH": {"value": "{}:{}".format(os.path.join(self.install_path, "bin"),
                                                                      os.path.join(os.path.expanduser('~'),
                                                                                   ".npm_modules", "bin"))}})
-        UI.delayed_display(DisplayMessage(_("You need to restart your current shell session for your {} installation "
+        UI.delayed_display(DisplayMessage(_("You may need to log back in for your {} installation "
                                             "to work properly").format(self.name)))
 
     def install_framework_parser(self, parser):
