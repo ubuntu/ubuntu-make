44f2888971122a6cbf6b22d43a82689a8a2e9e80
Galileo Sartor <galileo.sartor@gmail.com>
2016-10-07 22:07:26 +0200

    Add check to download_url. If None raise IndexError

--- umake/frameworks/ide.py
+++ umake/frameworks/ide.py
@@ -777,11 +777,12 @@ class LightTable(umake.frameworks.baseinstaller.BaseInstaller):
 
         try:
             assets = json.loads(page.buffer.read().decode())["assets"]
+            download_url = None
             for asset in assets:
                 if "linux" in asset["browser_download_url"]:
                     download_url = asset["browser_download_url"]
-                else:
-                    raise IndexError
+            if not download_url:
+                raise IndexError
         except (json.JSONDecodeError, IndexError):
             logger.error("Can't parse the download URL from the download page.")
             UI.return_main_screen(status_code=1)
@@ -822,11 +823,12 @@ class Atom(umake.frameworks.baseinstaller.BaseInstaller):
 
         try:
             assets = json.loads(page.buffer.read().decode())["assets"]
+            download_url = None
             for asset in assets:
                 if "tar.gz" in asset["browser_download_url"]:
                     download_url = asset["browser_download_url"]
-                else:
-                    raise IndexError
+            if not download_url:
+                raise IndexError
         except (json.JSONDecodeError, IndexError):
             logger.error("Can't parse the download URL from the download page.")
             UI.return_main_screen(status_code=1)
