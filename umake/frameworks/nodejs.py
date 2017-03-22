99ee74eb12cce159ac980e9285610f29e35c415b
Galileo Sartor <galileo.sartor@gmail.com>
2017-03-22 10:17:53 +0100

    Make nodejs read the download page instead of fixed shasum files

--- umake/frameworks/nodejs.py
+++ umake/frameworks/nodejs.py
@@ -24,10 +24,12 @@ from contextlib import suppress
 from gettext import gettext as _
 import logging
 import os
+import re
 import subprocess
 import umake.frameworks.baseinstaller
+from umake.network.download_center import DownloadCenter, DownloadItem
 from umake.interactions import DisplayMessage
-from umake.tools import get_current_arch, add_env_to_user, ChecksumType
+from umake.tools import get_current_arch, add_env_to_user, ChecksumType, MainLoop
 from umake.ui import UI
 
 logger = logging.getLogger(__name__)
@@ -45,7 +47,7 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
     def __init__(self, category):
         super().__init__(name="Nodejs Lang", description=_("Nodejs stable"), is_category_default=True,
                          category=category, only_on_archs=['i386', 'amd64'],
-                         download_page="https://nodejs.org/dist/latest/SHASUMS256.txt",
+                         download_page="https://nodejs.org/en/download/current",
                          checksum_type=ChecksumType.sha256,
                          dir_to_decompress_in_tarball="node*",
                          required_files_path=[os.path.join("bin", "node")])
@@ -54,6 +56,32 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
         "i386": "x86"
     }
 
+    def download_provider_page(self):
+        logger.debug("Download application provider page")
+        DownloadCenter([DownloadItem(self.download_page)], self.parse_shasum_page, download=False)
+
+    def parse_shasum_page(self, result):
+        """Parse the download page and get the SHASUMS256.txt page"""
+        logger.debug("Parse download metadata")
+
+        error_msg = result[self.download_page].error
+        if error_msg:
+            logger.error("An error occurred while downloading {}: {}".format(self.download_page, error_msg))
+            UI.return_main_screen(status_code=1)
+
+        url = False
+        for line in result[self.download_page].buffer:
+            line_content = line.decode()
+            with suppress(AttributeError):
+                shasum_url = re.search('a href="(.*SHASUMS\d\d\d\.txt\.asc)"', line_content).group(1)
+
+        if not result:
+            logger.error("Download page changed its syntax or is not parsable")
+            UI.return_main_screen(status_code=1)
+
+        self.download_page = shasum_url
+        DownloadCenter([DownloadItem(self.download_page)], self.get_metadata_and_check_license, download=False)
+
     def parse_download_link(self, line, in_download):
         """Parse Nodejs download link, expect to find a sha1 and a url"""
         url, shasum = (None, None)
@@ -61,8 +89,8 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
         if "linux-{}.tar.xz".format(self.arch_trans[arch]) in line:
             in_download = True
         if in_download:
-            url = self.download_page.strip("SHASUMS256.txt") + line.split(' ')[2].rstrip()
-            shasum = line.split(' ')[0]
+            url = self.download_page.strip("SHASUMS256.txt.asc") + line.split()[1].rstrip()
+            shasum = line.split()[0]
 
         if url is None and shasum is None:
             return (None, in_download)
@@ -70,7 +98,7 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
 
     def prefix_set(self):
         with suppress(IOError):
-            with open('{}/.npmrc'.format(os.path.expanduser('~')), 'r') as file:
+            with open(os.path.join(os.environ['HOME'], '.npmrc'), 'r') as file:
                 for line in file.readlines():
                     if line.startswith("prefix ="):
                         return True
@@ -79,7 +107,7 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
     def post_install(self):
         """Add nodejs necessary env variables and move module folder"""
         if not self.prefix_set():
-            with open('{}/.npmrc'.format(os.path.expanduser('~')), 'a+') as file:
+            with open(os.path.join(os.environ['HOME'], '.npmrc'), 'a+') as file:
                 file.write("prefix = ${HOME}/.npm_modules")
 
         add_env_to_user(self.name, {"PATH": {"value": "{}:{}".format(os.path.join(self.install_path, "bin"),
@@ -95,6 +123,6 @@ class NodejsLang(umake.frameworks.baseinstaller.BaseInstaller):
 
     def run_for(self, args):
         if args.lts:
-            self.download_page = "https://nodejs.org/dist/latest-argon/SHASUMS256.txt"
+            self.download_page = "https://nodejs.org/en/download/"
         print('Download from {}'.format(self.download_page))
         super().run_for(args)
