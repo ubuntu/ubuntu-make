e83429e50c691fb111987b3ee0c49b8943a18c2d
Galileo Sartor <galileo.sartor@gmail.com>
2017-03-29 22:49:34 +0200

    Fix medium tests for netbeans

--- tests/medium/test_ide.py
+++ tests/medium/test_ide.py
@@ -327,7 +327,7 @@ class BaseNetBeansInContainer(ContainerTests, test_ide.BaseNetBeansTests):
 
     TIMEOUT_START = 20
     TIMEOUT_STOP = 10
-    TEST_CHECKSUM_NETBEANS_DATA = "1e07ec8775939ba6d35731831bdb7bf0"
+    TEST_CHECKSUM_NETBEANS_DATA = "e5018c428b5657b669155886f58429af63f69436625e876be9710824bbfd144a"
 
     def setUp(self):
         self.hosts = {80: ["download.netbeans.org"], 443: ["netbeans.org"]}
@@ -349,7 +349,7 @@ class BaseNetBeansInContainer(ContainerTests, test_ide.BaseNetBeansTests):
     def test_install_with_changed_download_reference_page(self):
         """Installing NetBeans ide should fail if download reference page has significantly changed"""
         download_page_file_path = os.path.join(get_data_dir(), "server-content", "netbeans.org", "images_www",
-                                               "v6", "download", "8.0.42", "final", "js", "files.js")
+                                               "v6", "download", "8.2", "final", "js", "files.js")
         umake_command = self.command('{} ide netbeans'.format(UMAKE))
         self.bad_download_page_test(umake_command, download_page_file_path)
         self.assertFalse(self.launcher_exists_and_is_pinned(self.desktop_filename))
@@ -358,7 +358,7 @@ class BaseNetBeansInContainer(ContainerTests, test_ide.BaseNetBeansTests):
     def test_install_with_changed_checksum_page(self):
         """Installing NetBeans ide should fail if checksum link is wrong"""
         download_page_file_path = os.path.join(get_data_dir(), "server-content", "netbeans.org", "images_www",
-                                               "v6", "download", "8.0.42", "final", "js", "files.js")
+                                               "v6", "download", "8.2", "final", "js", "files.js")
         with swap_file_and_restore(download_page_file_path) as content:
             with open(download_page_file_path, "w") as newfile:
                 newfile.write(content.replace(self.TEST_CHECKSUM_NETBEANS_DATA, "abcdef"))
