61d82b6830d6fa96482044d8059ec63e1e7fd6ea
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2019-07-22 00:00:54 +0300

    Added terraform support

---
+++ tests/large/test_devops.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Canonical
+#
+# Authors:
+#  Didier Roche
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Tests for the Devops category"""
+import logging
+import platform
+import subprocess
+import os
+import tempfile
+from tests.large import LargeFrameworkTests
+from tests.tools import UMAKE, spawn_process
+
+logger = logging.getLogger(__name__)
+
+
+class TerraformTests(LargeFrameworkTests):
+    """Tests for Terraform"""
+
+    TIMEOUT_INSTALL_PROGRESS = 120
+    TIMEOUT_START = 20
+    TIMEOUT_STOP = 20
+
+    def setUp(self):
+        super().setUp()
+        self.installed_path = os.path.join(self.install_base_path, "devops", "terraform")
+        self.command_args = '{} devops terraform'.format(UMAKE)
+
+    @property
+    def exec_path(self):
+        return os.path.join(self.installed_path, "terraform")
+
+    def test_default_install(self):
+        self.child = spawn_process(self.command(self.command_args))
+        self.expect_and_no_warn("Choose installation path: {}".format(self.installed_path))
+        self.child.sendline("")
+        self.expect_and_no_warn("Installation done", timeout=self.TIMEOUT_INSTALL_PROGRESS)
+        self.wait_and_close()
+
+        self.assert_exec_exists()
+
+        # launch it, send SIGTERM and check that it exits fine
+        proc = subprocess.Popen(self.command_as_list(self.exec_path), stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+        self.check_and_kill_process([self.exec_path], wait_before=self.TIMEOUT_START, send_sigkill=True)
+        proc.wait(self.TIMEOUT_STOP)
+
+        # ensure that it's detected as installed:
+        self.child = spawn_process(self.command(self.command_args))
+        self.expect_and_no_warn("Terraform is already installed.*\[.*\] ")
+        self.child.sendline()
+        self.wait_and_close()
+        self.assert_exec_exists()
---
+++ umake/frameworks/devops.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Canonical
+#
+# Authors:
+#  Alexander Sidorenko
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+"""Devops module"""
+
+from contextlib import suppress
+from functools import partial
+from gettext import gettext as _
+import json
+import logging
+import os
+import platform
+import re
+import umake.frameworks.baseinstaller
+from umake.interactions import Choice, TextWithChoices, DisplayMessage
+from umake.network.download_center import DownloadItem
+from umake.ui import UI
+from umake.tools import create_launcher, get_application_desktop_file, MainLoop,\
+    get_current_arch, add_env_to_user
+
+logger = logging.getLogger(__name__)
+
+
+class DevopsCategory(umake.frameworks.BaseCategory):
+
+    def __init__(self):
+        super().__init__(name="devops", description=_("Devops Environment"), logo_path=None)
+
+
+class Terraform(umake.frameworks.baseinstaller.BaseInstaller):
+
+    def __init__(self, **kwargs):
+        super().__init__(name="Terraform", description=_("Infrastructure-as-code tool"),
+                         only_on_archs=['i386', 'amd64'],
+                         download_page="https://api.github.com/repos/hashicorp/terraform/releases/latest",
+                         dir_to_decompress_in_tarball=".",
+                         required_files_path=["terraform"],
+                         json=True, **kwargs)
+
+    arch_trans = {
+        "amd64": "amd64",
+        "i386": "386"
+    }
+
+    def parse_download_link(self, line, in_download):
+        """Parse Terraform download links"""
+        url = None
+        logger.info(line)
+        tag_name = line["tag_name"]
+        if tag_name:
+            version = tag_name[1:]
+            arch = self.arch_trans[get_current_arch()]
+            url = "https://releases.hashicorp.com/terraform/{version}/terraform_{version}_linux_{arch}.zip".format(
+                version=version, arch=arch)
+            logger.debug("Download url for terraform of version {version} is: {url}".format(version=version, url=url))
+        return url, in_download
+
+    def post_install(self):
+        """Add Terraform necessary env variables"""
+        add_env_to_user(self.name, {"PATH": {"value": os.path.join(self.install_path)}})
+        UI.delayed_display(DisplayMessage(self.RELOGIN_REQUIRE_MSG.format(self.name)))
