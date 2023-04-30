6dbfb847480cb12682624ea2dbb826b2ec0f985b
Galileo Sartor <galileo.sartor@gmail.com>
2023-04-30 14:45:04 +0200

    Enable completions

--- umake/__init__.py
+++ umake/__init__.py
@@ -17,6 +17,8 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+#PYTHON_ARGCOMPLETE_OK
+
 import argparse
 import gettext
 from gettext import gettext as _
